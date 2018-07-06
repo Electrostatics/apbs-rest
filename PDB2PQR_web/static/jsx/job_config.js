0c9a6359247f0235403e8aa346642defd2ad4a6f
Offor <elvis.offor@pnnl.gov>
2018-07-06 12:36:43 -0700

    Introduced Bootstrap and associated JS libraries. Added sample JSS

---
+++ PDB2PQR_web/static/jsx/job_config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class Button extends React.Component{}
+class SubmitButton extends React.Component{}
+
+class ButtonWindow extends React.Component{
+    constructor(props)
+}
+
+class Configuration extends React.Component{
+    constructor(props){
+        this.state = { jobtype: null }
+    }
+    render(){
+        if (this.state.jobtype === null){
+            
+        }
+    }
+}
