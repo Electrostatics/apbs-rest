3b95fb9062c2909792854587b756fe191a000517
Offor <elvis.offor@pnnl.gov>
2018-07-05 17:53:19 -0700

    Additional setup for flask framework

--- PDB2PQR_web/views.py
+++ PDB2PQR_web/views.py
@@ -2,5 +2,12 @@ from flask import render_template
 from PDB2PQR_web import app
 
 @app.route('/')
-def hello_world():
-    return "Hello, world!!!"
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+# @app.route('pdb2pqr')
+# def pdb2pqr_config():
+
+# @app.route('/apbs')
+# def apbs_config():
