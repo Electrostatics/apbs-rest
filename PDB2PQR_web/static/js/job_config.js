a89d2dda17a19a6773c94fdf22d4971edc3d5924
Offor <elvis.offor@pnnl.gov>
2018-07-06 18:50:13 -0700

    Renders button groups. Making progress on creating the landing page

--- PDB2PQR_web/static/js/job_config.js
+++ PDB2PQR_web/static/js/job_config.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// class SubmitButton extends React.Component{}
+// class FormWindowHeader extends React.Component{}
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,44 +11,48 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Button = function (_React$Component) {
-    _inherits(Button, _React$Component);
+var SelectJob = function (_React$Component) {
+    _inherits(SelectJob, _React$Component);
 
-    function Button() {
-        _classCallCheck(this, Button);
+    function SelectJob() {
+        _classCallCheck(this, SelectJob);
 
-        return _possibleConstructorReturn(this, (Button.__proto__ || Object.getPrototypeOf(Button)).apply(this, arguments));
+        return _possibleConstructorReturn(this, (SelectJob.__proto__ || Object.getPrototypeOf(SelectJob)).apply(this, arguments));
     }
 
-    return Button;
+    _createClass(SelectJob, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "btn-group" },
+                React.createElement(
+                    "button",
+                    { type: "button", className: "btn btn-primary" },
+                    "PDB2PQR"
+                ),
+                React.createElement(
+                    "button",
+                    { type: "button", className: "btn btn-primary" },
+                    "APBS"
+                )
+            );
+        }
+    }]);
+
+    return SelectJob;
 }(React.Component);
 
-var SubmitButton = function (_React$Component2) {
-    _inherits(SubmitButton, _React$Component2);
+function Button(props) {
+    return React.createElement(
+        "button",
+        { type: "button", className: props.buttonType, onClick: props.onClick },
+        props.value
+    );
+}
 
-    function SubmitButton() {
-        _classCallCheck(this, SubmitButton);
-
-        return _possibleConstructorReturn(this, (SubmitButton.__proto__ || Object.getPrototypeOf(SubmitButton)).apply(this, arguments));
-    }
-
-    return SubmitButton;
-}(React.Component);
-
-var FormWindowHeader = function (_React$Component3) {
-    _inherits(FormWindowHeader, _React$Component3);
-
-    function FormWindowHeader() {
-        _classCallCheck(this, FormWindowHeader);
-
-        return _possibleConstructorReturn(this, (FormWindowHeader.__proto__ || Object.getPrototypeOf(FormWindowHeader)).apply(this, arguments));
-    }
-
-    return FormWindowHeader;
-}(React.Component);
-
-var FormWindow = function (_React$Component4) {
-    _inherits(FormWindow, _React$Component4);
+var FormWindow = function (_React$Component2) {
+    _inherits(FormWindow, _React$Component2);
 
     function FormWindow() {
         _classCallCheck(this, FormWindow);
@@ -54,12 +61,27 @@ var FormWindow = function (_React$Component4) {
     }
 
     _createClass(FormWindow, [{
+        key: "renderButton",
+        value: function renderButton(txt) {
+            var _this3 = this;
+
+            return React.createElement(Button, {
+                value: txt,
+                buttonType: "btn btn-primary",
+                onClick: function onClick() {
+                    return _this3.props.onClick(txt);
+                }
+            });
+        }
+    }, {
         key: "render",
         value: function render() {
             return React.createElement(
-                MyButton,
-                { color: "blue", shadowSize: 2 },
-                "Click Me"
+                "div",
+                { className: "btn-group" },
+                this.renderButton("PDB2PQR"),
+                this.renderButton("APBS"),
+                this.renderButton("APBSSKLADFH")
             );
         }
     }]);
@@ -67,30 +89,54 @@ var FormWindow = function (_React$Component4) {
     return FormWindow;
 }(React.Component);
 
-var Configuration = function (_React$Component5) {
-    _inherits(Configuration, _React$Component5);
+var Configuration = function (_React$Component3) {
+    _inherits(Configuration, _React$Component3);
 
     function Configuration(props) {
         _classCallCheck(this, Configuration);
 
-        var _this5 = _possibleConstructorReturn(this, (Configuration.__proto__ || Object.getPrototypeOf(Configuration)).call(this, props));
+        var _this4 = _possibleConstructorReturn(this, (Configuration.__proto__ || Object.getPrototypeOf(Configuration)).call(this, props));
 
-        _this5.state = {
-            jobtype: null,
-            pdb2pqr_settings: null,
-            apbs_settings: null
+        _this4.state = {
+            job_type: null,
+            pdb2pqr_settings: {
+                pdb_id: null,
+                ff: null,
+                output_scheme: null
+            },
+            apbs_settings: {}
         };
-        return _this5;
+        return _this4;
     }
 
     _createClass(Configuration, [{
+        key: "selectJobClick",
+        value: function selectJobClick(jobtype) {
+            this.setState({
+                job_type: jobtype
+            });
+            console.log(this.state.job_type);
+        }
+    }, {
         key: "render",
         value: function render() {
-            if (this.state.jobtype === null) {} else if (this.state.jobtype == "pdb2pqr") {} else if (this.state.jobtype == "apbs") {}
+            var _this5 = this;
+
+            if (this.state.jobtype == null) {
+                var blah = React.createElement(FormWindow, { onClick: function onClick(j) {
+                        return _this5.selectJobClick(j);
+                    } });
+                return blah;
+            } else if (this.state.jobtype == "pdb2pqr") {} else if (this.state.jobtype == "apbs") {
+                console.log("selected apbs");
+            }
         }
     }]);
 
     return Configuration;
 }(React.Component);
+
+// let domContainer = );
+
 
 ReactDOM.render(React.createElement(Configuration, null), document.getElementById('choose_job'));
--- PDB2PQR_web/static/jsx/job_config.js
+++ PDB2PQR_web/static/jsx/job_config.js
@@ -1,37 +1,81 @@
 'use strict';
 
-class Button extends React.Component{
+// class SubmitButton extends React.Component{}
+// class FormWindowHeader extends React.Component{}
 
-}
-class SubmitButton extends React.Component{}
-class FormWindowHeader extends React.Component{}
-
-class FormWindow extends React.Component{
+class SelectJob extends React.Component{
     render(){
         return(
-            <MyButton color="blue" shadowSize={2}>
-                Click Me
-            </MyButton>
+            <div className="btn-group">
+                <button type="button" className="btn btn-primary">PDB2PQR</button>
+                <button type="button" className="btn btn-primary">APBS</button>
+            </div>
         )
-    }    
+    };
+}
+
+function Button(props){
+    return(
+        <button type="button" className={props.buttonType} onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+class FormWindow extends React.Component{
+    renderButton(txt){
+        return(
+            <Button
+                value={txt}
+                buttonType="btn btn-primary"
+                onClick={() => this.props.onClick(txt)}
+            />
+        )
+    }
+
+    render(){
+        return(
+            <div className="btn-group">
+                {this.renderButton("PDB2PQR")}
+                {this.renderButton("APBS")}
+                {this.renderButton("APBSSKLADFH")}
+            </div>
+        )
+    }
 }
 
 class Configuration extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            jobtype: null,
-            pdb2pqr_settings: null,
-            apbs_settings: null,
+            job_type: null,
+            pdb2pqr_settings: {
+                pdb_id: null,
+                ff: null,
+                output_scheme: null,
+            },
+            apbs_settings: {},
         };
     }
+
+    selectJobClick(jobtype){
+        this.setState({
+            job_type: jobtype
+        })
+        console.log(this.state.job_type)
+    }
+
     render(){
-        if (this.state.jobtype === null){
-            
+        if (this.state.jobtype == null){
+            let blah = <FormWindow onClick={j => this.selectJobClick(j)}/>
+            return(
+                blah
+            );
         }
         else if (this.state.jobtype == "pdb2pqr"){}
-        else if (this.state.jobtype == "apbs"){}
+        else if (this.state.jobtype == "apbs"){console.log("selected apbs")}
     }
 }
 
-ReactDOM.render(<Configuration />, document.getElementById('choose_job'))
+// let domContainer = );
+ReactDOM.render(<Configuration />, document.getElementById('choose_job'));
