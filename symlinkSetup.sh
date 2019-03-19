88af6454d46d767779e00592dc8febd74e683c32
Offor <elvis.offor@pnnl.gov>
2019-03-19 15:59:06 -0700

    Added symlinkSetup to alleviate headaches

---
+++ symlinkSetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Used specifically with directory structure for Elvis
+# Not guaranteed to work for the directory structure of whoever clones this
+# Directory structure I'm working with (everything listed below is a dir):
+#   +-- Parent_directory
+#   |   +-- react_flask_pdb2pqr (root directory of repo)
+#   |       +-- PDB2PQR_web
+#   |
+#   |   +-- antd_pdb2pqr
+#   |       +-- build
+#   |
+#   |   +-- builds
+#   |       +-- pdb2pqr_build
+
+
+# This script sets up the symbolic links in order for development to be easier
+
+ln -s ../builds/pdb2pqr_build ./pdb2pqr_build
+
+cd PDB2PQR_web
+ln -s ../../antd_pdb2pqr/build ./build
+cd ..
+
+cd ../builds/pdb2pqr_build
+ln -s ../../react_flask_pdb2pqr/main_cgi.py main_cgi.py
+ln -s ../../react_flask_pdb2pqr/querystatus.py querystatus.py
+# ln -s ../../react_flask_pdb2pqr/pdb2pqr.py pdb2pqr.py
+cd ../../react_flask_pdb2pqr
