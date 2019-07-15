443dae63259fdbe1a1ab6db73b9d3ec7b05dbc8b
Offor <elvis.offor@pnnl.gov>
2019-07-15 16:09:00 -0700

    Updated testing script for REST services

--- tests/test_rest_services.py
+++ tests/test_rest_services.py
@@ -28,7 +28,7 @@ def check_environment_vars():
     if TASK_HOST == '': raise EnvironmentValueError('TASK_HOST')
     if EXEC_HOST == '': raise EnvironmentValueError('EXEC_HOST')
     if STORAGE_HOST == '': raise EnvironmentValueError('STORAGE_HOST')
-    # if WORKFLOW_HOST == '': raise EnvironmentValueError('WORKFLOW_HOST')
+    if WORKFLOW_HOST == '': raise EnvironmentValueError('WORKFLOW_HOST')
     # raise ValueError()
 
 
@@ -101,52 +101,83 @@ def monitor_task_status(job_id, task_name, wait=False):
     return status[task_name]['status'], file_names
 
 
-def run_job():
-    pass
+def submit_workflow(job_id, workflow_str, workflow_params=None, task_files=None, use_infile=None):
+    if use_infile is True:
+        response = post('%s/api/workflow/%s?infile=%s' % (WORKFLOW_HOST, job_id, str(use_infile)), json=workflow_params)
+    else:
+        request_json = {
+            'workflow': workflow_str,
+            'form': workflow_params
+        }
+        response = post('%s/api/workflow/%s' % (WORKFLOW_HOST, job_id), json=request_json)
+    return response
+
+def get_workflow_task_status(job_id, task_name, wait=False):
+    response = get('%s/api/workflow/%s/%s?wait=%s' % (WORKFLOW_HOST, job_id, task_name, wait) )
+    return response
+
+def monitor_workflow_status(job_id, task_name, wait=False):
+    file_names = []
+
+    if wait == True:
+        # Long-poll server to wait until task is complete
+        # response = get_task_status(job_id, task_name, wait=True)
+        response = get_workflow_task_status(job_id, task_name, wait=True)
+        status = loads(response.content)
+        if status[task_name]['status'] == 'complete':
+            status = loads(response.content)
+            file_names = status[task_name]['files']
+    else:
+        # Request status once-per-second until task is complete
+        while(True):
+            sleep(1)
+            print('.', end='', flush=True)
+            # response = get_task_status(job_id, task_name)
+            response = get_workflow_task_status(job_id, task_name)
+            status = loads(response.content)
+
+            if status[task_name]['status'] == 'complete':
+                sleep(1)
+                # response = get_task_status(job_id, task_name)
+                response = get_workflow_task_status(job_id, task_name)
+                status = loads(response.content)
+
+                file_names = status[task_name]['files']
+                # pprint(status)
+                break
+
+
+    print()    
+    return status[task_name]['status'], file_names
+
 
 # Tests Workflow Submitter service with PDB2PQR
-def start_pdb2pqr_workflow():
+def start_pdb2pqr_workflow(pdb_id=None):
     print('------PDB2PQR Workflow Submit------')
 
-    print('  getting job ID')
+    if pdb_id is None:
+        pdb_id = '1a1p'
+
+    print('  getting job ID', end='', flush=True)
     job_id = get_job_id()
-    task_name = 'pdb2pqr'
+    print('... %s' % job_id)
+    
+    # workflow_str = ['pdb2pqr']
+    # workflow_str = ['pdb2pqr', 'apbs']
+    workflow_str = 'pdb2pqr'
+
     task_params = {
         "DEBUMP"        : "atomsnotclose",
         "FF"            : "parse",
         "FFOUT"         : "internal",
         "INPUT"         : "makeapbsin",
         "OPT"           : "optimizeHnetwork",
-        "PDBID"         : "1abf",
-        # "PDBID"         : "1a1p",
+        "PDBID"         : pdb_id,
         "PDBSOURCE"     : "ID",
         "PH"            : "7.0",
         "PKACALCMETHOD" : "propka"
     }
 
-    print('  submitting workflow parmaeters ')
-    r = submit_task(job_id, task_name, task_params=task_params)
-    # pprint(r)
-    
-    print('  obtaining task status')
-    status = get_task_status(job_id, task_name)
-
-    print('  monitoring task status', end='', flush=True)
-    status, file_names = monitor_task_status(job_id, task_name)
-
-    print('  task done; printing output file names\n')
-    pprint(file_names, indent=2)
-
-    print('-----------------------------------')
-
-
-# Tests Task Submitter service with PDB2PQR
-def start_pdb2pqr_task():
-    print('------PDB2PQR Task Submit------')
-
-    print('  getting job ID')
-    job_id = get_job_id()[:8]
-    task_name = 'pdb2pqr'
     task_params = {
         "DEBUMP"        : "atomsnotclose",
         "FF"            : "parse",
@@ -155,13 +186,126 @@ def start_pdb2pqr_task():
         "OPT"           : "optimizeHnetwork",
         # "PDBID"         : "1abf",
         "PDBID"         : "1a1p",
+        # "PDBID"         : pdb_id,
+        "PDBSOURCE"     : "ID",
+        "PH"            : "7.0",
+        "PKACALCMETHOD" : "propka"
+    }
+    # task_params = [{
+    #     "DEBUMP"        : "atomsnotclose",
+    #     "FF"            : "parse",
+    #     "FFOUT"         : "internal",
+    #     "INPUT"         : "makeapbsin",
+    #     "OPT"           : "optimizeHnetwork",
+    #     # "PDBID"         : "1abf",
+    #     "PDBID"         : "1a1p",
+    #     # "PDBID"         : pdb_id,
+    #     "PDBSOURCE"     : "ID",
+    #     "PH"            : "7.0",
+    #     "PKACALCMETHOD" : "propka"
+    # },
+    # {   
+    #     u'bcfl': u'sdh',
+    #     u'calcenergy': u'total',
+    #     u'calcforce': u'no',
+    #     u'cgcent': u'mol',
+    #     u'cgcentid': 1,
+    #     u'cglenx': 42.6292,
+    #     u'cgleny': 33.078599999999994,
+    #     u'cglenz': 25.117499999999996,
+    #     u'charge0': u'',
+    #     u'charge1': u'',
+    #     u'charge2': u'',
+    #     u'chgm': u'spl2',
+    #     u'conc0': u'',
+    #     u'conc1': u'',
+    #     u'conc2': u'',
+    #     u'dimenx': 97,
+    #     u'dimeny': 65,
+    #     u'dimenz': 65,
+    #     u'fgcent': u'mol',
+    #     u'fgcentid': 1,
+    #     u'fglenx': 42.6292,
+    #     u'fgleny': 33.078599999999994,
+    #     u'fglenz': 25.117499999999996,
+    #     u'gcent': u'molecule',
+    #     u'glenx': 42.6292,
+    #     u'gleny': 33.078599999999994,
+    #     u'glenz': 25.117499999999996,
+    #     u'hiddencheck': u'local',
+    #     u'mol': u'1',
+    #     u'ofrac': 0.1,
+    #     u'output_scalar': [   u'writepot'],
+    #     u'pdb2pqrid': job_id,
+    #     u'pdie': 2,
+    #     u'pdimex': 1,
+    #     u'pdimey': 1,
+    #     u'pdimez': 1,
+    #     u'radius0': u'',
+    #     u'radius1': u'',
+    #     u'radius2': u'',
+    #     u'sdens': 10,
+    #     u'sdie': 78.54,
+    #     u'solvetype': u'lpbe',
+    #     u'srad': 1.4,
+    #     u'srfm': u'smol',
+    #     u'swin': 0.3,
+    #     u'temp': 298.15,
+    #     u'type': u'mg-auto',
+    #     u'writeformat': u'dx'
+    # }]    
+
+    print('  submitting workflow parmaeters ')
+    r = submit_workflow(job_id, workflow_str, workflow_params=task_params)
+    # pprint(r)
+    
+    print('  obtaining workflow task status')
+    # # status = get_task_status(job_id, task_name)
+    task_name = workflow_str
+    status = get_workflow_task_status(job_id, task_name)
+    # status = get_task_status(job_id, task_name)
+
+    print('  monitoring workflow status', end='', flush=True)
+    status, file_names = monitor_workflow_status(job_id, task_name)
+    # status, file_names = monitor_task_status(job_id, task_name)
+
+    print('  task done; printing output file names\n')
+    pprint(file_names, indent=2)
+
+    print('-----------------------------------')
+
+
+# Tests Task Submitter service with PDB2PQR
+def start_pdb2pqr_task(pdb_id=None):
+    print('------PDB2PQR Task Submit------')
+
+    if pdb_id is None:
+        pdb_id = '1a1p'
+
+    print('  getting job ID', end='', flush=True)
+    job_id = get_job_id()
+    print('... %s' % job_id)
+
+
+    task_name = 'pdb2pqr'
+    task_params = {
+        "DEBUMP"        : "atomsnotclose",
+        "FF"            : "parse",
+        "FFOUT"         : "internal",
+        "INPUT"         : "makeapbsin",
+        "OPT"           : "optimizeHnetwork",
+        # "PDBID"         : "1abf",
+        # "PDBID"         : "1a1p",
+        "PDBID"         : pdb_id,
         "PDBSOURCE"     : "ID",
         "PH"            : "7.0",
         "PKACALCMETHOD" : "propka"
     }
 
-    print('  sending task to run')
+    # print('  sending task to run')
+    print('  sending task to run  ', end='')
     r = submit_task(job_id, task_name, task_params=task_params)
+    print(r.status_code)
     # pprint(r)
     
     print('  obtaining task status')
@@ -177,12 +321,13 @@ def start_pdb2pqr_task():
     print('-------------------------------')
 
 # Tests Task Submitter service with APBS
-def start_apbs_task():
+def start_apbs_task(job_id=None):
     print('--------APBS Task Submit--------')
 
 
     print('  getting job ID')
-    job_id = get_job_id()
+    if job_id is None:
+        job_id = get_job_id()
     task_name = 'apbs'
 
     in_file_name = 'apbs_upload/sample.in'
@@ -272,8 +417,13 @@ if __name__ == "__main__":
     '''Run sample PDB2PQR job'''
 
     # start_pdb2pqr_task()
+    start_pdb2pqr_task(pdb_id='1hhj')
 
-    start_apbs_task()
+    start_pdb2pqr_workflow(pdb_id='1hhj')
+    # start_pdb2pqr_workflow(pdb_id='1a1p')
+    # start_pdb2pqr_task(pdb_id='1a1p')
+
+    # start_apbs_task()
 
     # get job ID
     # job_id = get_job_id()
