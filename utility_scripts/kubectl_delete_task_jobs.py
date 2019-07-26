0b8277ee7e9a4400df3624eb2db66de4ecb1cb9a
elvis <eoffor21@gmail.com>
2019-07-25 17:57:50 -0700

    Some cleanup scripts to remove TESK task pods/jobs

---
+++ utility_scripts/kubectl_delete_task_jobs.py
@@ -0,0 +1,25 @@
+import subprocess
+
+if __name__ == "__main__":
+    
+    # Get kubectl pods
+    command = 'kubectl get jobs'
+    p1 = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+
+    # Parse kubectl pods which begin with "task-"
+    command = 'grep ^task-'
+    p2 = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stdin=p1.stdout)
+
+    output, err = p2.communicate()
+    # print(output)
+
+    # Remove dead pods from cluster
+    if output:
+        for line in output.split('\n'):
+            spl_line = line.split()
+            if len(spl_line) > 0:
+                task_name = spl_line[0]
+                command = 'kubectl delete jobs %s' % spl_line[0]
+                p3 = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+                output, err = p3.communicate()
+                print(output.strip())
