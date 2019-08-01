8767e5b7abb370cadacb46ae2484751368f5ba25
Offor <elvis.offor@pnnl.gov>
2019-08-01 16:50:04 -0700

    Added comments; removed dead code

--- src/v2_workflow/service/workflow_service.py
+++ src/v2_workflow/service/workflow_service.py
@@ -3,12 +3,8 @@ from os import getenv
 from sys import stderr
 from requests import get, post
 
-# from . import workflow_utils
-import time, json
-
 workflow_app = Blueprint('workflow_app', __name__)
 
-# WORKFLOW_DATABASE = getenv('WORKFLOW_DATABASE', 'http://localhost:3306')
 END_STATES = ['complete', 'error', None]
 TASK_HOST = getenv('TASK_HOST')
 
@@ -20,47 +16,14 @@ def is_alive():
 @workflow_app.route('/api/workflow/<job_id>/<task_name>', methods=['GET', 'POST', 'OPTIONS'])
 @workflow_app.route('/api/workflow/<job_id>', methods=['GET', 'POST', 'OPTIONS'])
 def submit_workflow_request(job_id, task_name=None):
-    # 
     response = {}
     http_status_code = None
 
-    # from pprint import pprint
-    # pprint(request.data)
-
     if request.method == 'POST':
-        '''==========================================='''
-        '''==========================================='''
-        # try:
-        #     request_json = request.get_json()
-        #     form_list = request_json['form']
-        #     if 'workflow' in request_json:
-        #         task_list = request_json['workflow']
-        #     elif task_name is not None:
-        #         task_list = [task_name]
-        #     else:
-        #         http_status_code = 400
-        #         response = "Request missing the 'workflow' parameter in JSON, or you did not specify a task in the URL."
-        #         return response, http_status_code
-            
-        #     # Submit each task directly to the task service
-        #     #TODO: setup a workflow service using TES
-        #     http_status_code = 202
-        #     for task, form in zip(task_list, form_list):
-        #         task_response = post('%s/api/task/%s/%s' % (TASK_HOST, job_id, task), json=json.dumps(form) )
-        #         if task_response.status_code != 202:
-        #             http_status_code = 202
-        #             response = task_response.json()
-
-        # except Exception as e:
-        #     print(e)
-        #     http_status_code = 500
-
-        '''==========================================='''
-        '''==========================================='''
-
         try:
             request_json = request.get_json()
 
+            # Returns a bad request error if missing 1) both workflow name *and* task name or 2) form data
             if ('workflow' not in request_json and task_name is None) or 'form' not in request_json:
                 http_status_code = 400
                 response['error'] = ""
@@ -71,6 +34,7 @@ def submit_workflow_request(job_id, task_name=None):
                 if task_name is None:
                     response['error'] = "Did not specify a task_name in the URL. This or a 'workflow' param in the JSON is required\n"
 
+            # Dubs the workflow_name as the passed task_name if missing in request
             if 'workflow' in request_json:                
                 workflow_name = request_json['workflow']
             else:
@@ -79,39 +43,44 @@ def submit_workflow_request(job_id, task_name=None):
             task_params = request_json['form']
             http_status_code = 202
             
+            # Send the appropriate task parameters to the Task Service
             print(f"sending workflow '{workflow_name}' for job '{job_id}' to the task service")
             task_response = post('%s/api/task/%s/%s' % (TASK_HOST, job_id, workflow_name), json=task_params)
             if task_response.status_code == 202:
                 http_status_code = task_response.status_code
                 response = task_response.json()
+            #TODO: write handler for a fail case if Task Service sends non-202 response
+
         except Exception as e:
             http_status_code = 500
             print('Error: %s' % e, file=stderr)
 
-
-        '''==========================================='''
-        '''==========================================='''
-
-
     elif request.method == 'GET':
-        # if task_name not in ['apbs', 'pdb2pqr']:
-        #     response = {
-        #         'status': None,
-        #         'error': f"task type '{task_name}' does not exist or is not implemented"
-        #     }
-        #     http_status_code = 404
-        #     # return response, 404
-        #     print('happening here')
-        # else:
-        wait_on_task = False
-        if 'wait' in request.args.keys():
-            if request.args['wait'].lower() == 'true':
-                # return {'args': request.args['wait']}
-                wait_on_task = True
-        task_status_response = get('%s/api/task/%s/%s?wait=%s' % (TASK_HOST, job_id, task_name, wait_on_task))
+        '''
+            Workflow Service currently only fulfills APBS and PDB2PQR (single-task) 'workflows'
+            As such, GET request simply fetches the task status of the given task_name and
+                passes the respective response/status code to the client
+        '''
 
-        http_status_code = task_status_response.status_code
-        response = task_status_response.json()
+        # Construct error response if given invalid task
+        if task_name not in ['apbs', 'pdb2pqr']:
+            response = {
+                'status': None,
+                'error': f"task type '{task_name}' does not exist or is not implemented"
+            }
+            http_status_code = 404
+
+        # Get the task response from the Task Service, pass the response/status code
+        else:
+            wait_on_task = False
+            if 'wait' in request.args.keys():
+                if request.args['wait'].lower() == 'true':
+                    # return {'args': request.args['wait']}
+                    wait_on_task = True
+            task_status_response = get('%s/api/task/%s/%s?wait=%s' % (TASK_HOST, job_id, task_name, wait_on_task))
+
+            http_status_code = task_status_response.status_code
+            response = task_status_response.json()
 
         return response, http_status_code
             
@@ -119,4 +88,3 @@ def submit_workflow_request(job_id, task_name=None):
         http_status_code = 204
 
     return response, http_status_code
-
