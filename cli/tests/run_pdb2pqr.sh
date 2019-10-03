ab00447aa6704602dd9b1c4bd657a44c49cd1a4e
Offor <eoffor21@gmail.com>
2019-10-02 17:22:46 -0700

    Added CLI input samples

--- cli/tests/run_pdb2pqr.sh
+++ cli/tests/run_pdb2pqr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd output
-go run ../../pdb2pqr/pdb2pqr-cli.go --with-ph=7.0 --ph-calc-method=propka --apbs-input --ff=parse --verbose ../sample/1a1p.pdb 1a1p_out.pqr
+go run ../../pdb2pqr/pdb2pqr-cli.go --with-ph=7.0 --ph-calc-method=propka --apbs-input --ff=parse --verbose ../samples/1a1p.pdb 1a1p_out.pqr
 
 cd ..
