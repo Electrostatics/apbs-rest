8294663f8aa0e1551c3d7e863381a8b5fd5de642
Offor <eoffor21@gmail.com>
2019-10-22 15:56:02 -0700

    Check command line for port num; default is 9000

--- src/storage/start_minio.sh
+++ src/storage/start_minio.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
 MINIO_ACCESS_KEY=admin
 MINIO_SECRET_KEY=adminadmin
-docker run --rm -it -p 9000:9000 \
+
+if [ "$1" != "" ]
+then
+    echo "Port declared: $1"
+    PORT="$1"
+else
+    echo "No port specified. Default: 9000"
+    PORT="9000"
+fi
+
+docker run --rm -it -p 9090:9000 \
     -e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY\
     -e MINIO_SECRET_KEY=$MINIO_SECRET_KEY\
     minio/minio server /data
