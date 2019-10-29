9e7e4914f67cbcc4b6a0f3fb49c015ea43ae8b2b
Offor <eoffor21@gmail.com>
2019-10-29 12:33:12 -0700

    Fixed command line port argument not being used

--- src/storage/start_minio.sh
+++ src/storage/start_minio.sh
@@ -12,7 +12,7 @@ else
     PORT="9000"
 fi
 
-docker run --rm -it -p 9090:9000 \
+docker run --rm -it -p $PORT:9000 \
     -e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY\
     -e MINIO_SECRET_KEY=$MINIO_SECRET_KEY\
     minio/minio server /data
