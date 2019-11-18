e48ce960deb74236d1b8174a7e07aab8937807d8
Offor <eoffor21@gmail.com>
2019-11-18 11:00:02 -0800

    Disabled long-poll for period checks in task tests

--- tests/integration/common_assertions.py
+++ tests/integration/common_assertions.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, time
 
 def assert_task_status(task_url: str, job_id: str, jobtype: str):
     # GET: check status of running task
@@ -13,15 +13,25 @@ def assert_task_status(task_url: str, job_id: str, jobtype: str):
     assert json_dict[json_dict['jobtype']]['status'] == 'running'
 
 
-    # GET: check status of running task (long-poll)
-    response = requests.get('%s/%s/%s?wait=true' % (task_url, job_id, jobtype))
+    # # GET: check status of running task (long-poll)
+    # response = requests.get('%s/%s/%s?wait=true' % (task_url, job_id, jobtype))
+    # assert response.status_code == 200
+    # assert 'Content-Type' in response.headers.keys() and response.headers['Content-Type'] == 'application/json'
+    # json_dict = json.loads(response.content)
+
+    # assert json_dict['jobtype'] in json_dict.keys()
+    # assert set(['jobtype', 'jobid', json_dict['jobtype']]) == json_dict.keys()
+    # assert set(['status', 'startTime', 'endTime', 'files', 'inputFiles', 'outputFiles']) ==  json_dict[json_dict['jobtype']].keys()
+
+    # GET: continually check status of running task till completion
+    response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
+    json_dict = json.loads(response.content)
     assert response.status_code == 200
     assert 'Content-Type' in response.headers.keys() and response.headers['Content-Type'] == 'application/json'
-    json_dict = json.loads(response.content)
-
-    assert json_dict['jobtype'] in json_dict.keys()
-    assert set(['jobtype', 'jobid', json_dict['jobtype']]) == json_dict.keys()
-    assert set(['status', 'startTime', 'endTime', 'files', 'inputFiles', 'outputFiles']) ==  json_dict[json_dict['jobtype']].keys()
+    while json_dict[json_dict['jobtype']]['status'] == 'running':
+        time.sleep(1) # wait 1 second before checking status again
+        response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
+        json_dict = json.loads(response.content)
 
     # GET: check status of completed task
     response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
