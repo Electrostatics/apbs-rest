60e79e5847d58e7dff37733444430fcdb0422937
Offor <eoffor21@gmail.com>
2019-11-19 17:02:53 -0800

    Added 10-minute timeout for task/workflow tests

--- tests/integration/common_assertions.py
+++ tests/integration/common_assertions.py
@@ -1,6 +1,6 @@
 import requests, json, time
 
-def assert_task_status(task_url: str, job_id: str, jobtype: str):
+def assert_task_status(task_url: str, job_id: str, jobtype: str, timeout_minutes: int=10):
     # GET: check status of running task
     response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
     assert response.status_code == 200
@@ -28,10 +28,14 @@ def assert_task_status(task_url: str, job_id: str, jobtype: str):
     json_dict = json.loads(response.content)
     assert response.status_code == 200
     assert 'Content-Type' in response.headers.keys() and response.headers['Content-Type'] == 'application/json'
+    start_time = int(time.time())
     while json_dict[json_dict['jobtype']]['status'] == 'running':
         time.sleep(1) # wait 1 second before checking status again
         response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
         json_dict = json.loads(response.content)
+        cur_time = int(time.time())
+        if cur_time-start_time > (60*timeout_minutes):
+            raise TimeoutError("Test job '%s' (%s) took too long...exiting" % (job_id, jobtype))
 
     # GET: check status of completed task
     response = requests.get('%s/%s/%s' % (task_url, job_id, jobtype))
@@ -44,7 +48,7 @@ def assert_task_status(task_url: str, job_id: str, jobtype: str):
     assert set(['status', 'startTime', 'endTime', 'files', 'inputFiles', 'outputFiles']) ==  json_dict[json_dict['jobtype']].keys()
     assert json_dict[json_dict['jobtype']]['status'] == 'complete'
 
-def assert_workflow_status(workflow_url: str, job_id: str, jobtype: str):
+def assert_workflow_status(workflow_url: str, job_id: str, jobtype: str, timeout_minutes: int=10):
     # GET: check status of running task
     response = requests.get('%s/%s/%s' % (workflow_url, job_id, jobtype))
     assert response.status_code == 200
@@ -61,10 +65,14 @@ def assert_workflow_status(workflow_url: str, job_id: str, jobtype: str):
     json_dict = json.loads(response.content)
     assert response.status_code == 200
     assert 'Content-Type' in response.headers.keys() and response.headers['Content-Type'] == 'application/json'
+    start_time = int(time.time())
     while json_dict[json_dict['jobtype']]['status'] == 'running':
         time.sleep(1) # wait 1 second before checking status again
         response = requests.get('%s/%s/%s' % (workflow_url, job_id, jobtype))
         json_dict = json.loads(response.content)
+        cur_time = int(time.time())
+        if cur_time-start_time > (60*timeout_minutes):
+            raise TimeoutError("Test job '%s' (%s) took too long...exiting" % (job_id, jobtype))
 
     # GET: check status of completed task
     response = requests.get('%s/%s/%s' % (workflow_url, job_id, jobtype))
