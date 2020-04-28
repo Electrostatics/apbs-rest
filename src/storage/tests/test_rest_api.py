d9bca67be5b9d2d742e464509eeba9965237d503
marat valiev <marat.valiev@gmail.com>
2020-04-28 16:03:59 -0700

    Improve upload test.

--- src/storage/tests/test_rest_api.py
+++ src/storage/tests/test_rest_api.py
@@ -4,6 +4,8 @@
 
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
+from pathlib import Path
+
 """This is the module."""
 import filecmp
 import os
@@ -49,11 +51,11 @@ def gen_big_file(filename, size):
 
 
 @pytest.fixture(scope="module")
-def setup():
+def services():
     """
-    setup/teardown for storage rest service
+    setup/teardown for storage rest servicec
     """
-    print("entered setup")
+    print("creating services")
 
     try:
         subprocess.check_call(["docker-compose", "build"])
@@ -61,34 +63,90 @@ def setup():
         sleep(2)
         yield
     finally:
-        print("\n cleaning up")
+        print("\n stopping services")
         subprocess.check_call(["docker-compose", "down"])
 
 
-def test_flask_server(setup):
+@pytest.fixture(scope="module")
+def test_data(tmpdir_factory):
+    """
+    create data files
+    """
+    print("creating data files")
+
+    sizes = (100, 200, 300)
+
+    files = []
+    for size in sizes:
+        filename = F"file{size}"
+        filepath = tmpdir_factory.mktemp("data") / filename
+        gen_big_file(filepath, size)
+        files.append(Path(filepath))
+
+    assert True
+    return files
+
+
+def test_flask_server(services, test_data):
     r = requests.request("GET", FLASK_HOST)
 
     assert r.status_code == 200
 
 
-def test_upload(setup, tmp_path):
+def test_upload(services, test_data, tmp_path):
 
-    filepath = tmp_path / "bigfile1"
-    filename = filepath.name
+    test_files = test_data
 
-    gen_big_file(filepath, 100)
-    job_id = 124
-    url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
-    files = {'file_data': filepath.open('rb')}
-    r = requests.post(url, files=files)
-    assert r.status_code == 201
+    job_id = 12345
 
-    r = requests.get(url)
-    filepath1 = tmp_path / "bigfile2"
-    print("\n status code ", r.status_code)
-    with filepath1.open('wb') as f:
-        f.write(r.content)
+    for filepath in test_files:
+        filename = filepath.name
+        url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
+        files = {'file_data': filepath.open('rb')}
+        r = requests.post(url, files=files)
+        assert r.status_code == 201
 
-    assert filecmp.cmp(filepath1,filepath)
+    for filepath in test_files:
+        filename = filepath.name
+        url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
+        r = requests.get(url)
+        download_path = tmp_path / filename
+        assert r.status_code == 200
+        with download_path.open('wb') as f:
+            f.write(r.content)
 
+        assert filecmp.cmp(download_path, filepath)
+    #
 
+    # filepath = tmp_path / "bigfile1"
+    # filename = filepath.name
+    #
+    # gen_big_file(filepath, 100)
+    # job_id = 124
+    # url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
+    # files = {'file_data': filepath.open('rb')}
+    # r = requests.post(url, files=files)
+    # assert r.status_code == 201
+
+#
+# def test_upload(services, tmp_path):
+#
+#     filepath = tmp_path / "bigfile1"
+#     filename = filepath.name
+#
+#     gen_big_file(filepath, 100)
+#     job_id = 124
+#     url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
+#     files = {'file_data': filepath.open('rb')}
+#     r = requests.post(url, files=files)
+#     assert r.status_code == 201
+#
+#     r = requests.get(url)
+#     filepath1 = tmp_path / "bigfile2"
+#     print("\n status code ", r.status_code)
+#     with filepath1.open('wb') as f:
+#         f.write(r.content)
+#
+#     assert filecmp.cmp(filepath1,filepath)
+#
+#
