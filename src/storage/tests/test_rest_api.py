6f8902834f4fcc803fb61c3f9a3f73d52af5979e
marat valiev <marat.valiev@gmail.com>
2020-04-30 18:30:52 -0700

    Add flask health check as part test setup.

--- src/storage/tests/test_rest_api.py
+++ src/storage/tests/test_rest_api.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""This is the module."""
-
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
+""" Tests for rest api for storage service"""
+import logging
 import tarfile
 from pathlib import Path
+from time import time, sleep
 
-"""This is the module."""
 import filecmp
 import os
 import subprocess
@@ -15,8 +11,6 @@ import uuid
 from random import randint
 
 import requests
-from time import sleep
-
 import pytest
 
 FLASK_HOST = "http://localhost:5001"
@@ -24,6 +18,7 @@ STORAGE_ENDPOINT = FLASK_HOST + "/api/storage"
 
 # RESPONSE CODES
 DELETE_OK = 204
+
 
 def generate_big_random_bin_file(filename, size):
     """
@@ -40,7 +35,16 @@ def generate_big_random_bin_file(filename, size):
         fp.write(os.urandom(size * 1024 * 1024))
 
 
-def gen_big_file(filename, size):
+def generate_big_sparse_file(filename, size):
+    """
+    generate sparse binary file with the specified size in megabytes
+    :param filename: the filename
+    :param size: the size in megabytes
+    :return:void
+
+    note: adapted from
+    https://www.bswen.com/2018/04/python-How-to-generate-random-large-file-using-python.html
+    """
     size = size * 1024 * 1024
     f = open(filename, "wb")
     f.seek(randint(1, int(size / 2)))
@@ -56,52 +60,90 @@ def gen_big_file(filename, size):
 @pytest.fixture(scope="module")
 def services():
     """
-    setup/teardown for storage rest servicec
+    setup/teardown for storage rest services
     """
-    print("creating services")
+    logging.info("Bringing up services via docker-compose")
 
     try:
         subprocess.check_call(["docker-compose", "build"])
         subprocess.check_call(["docker-compose", "up", "-d"])
-        # sleep(2)
+        if not flask_ready_check():
+            raise IOError('Failed to bring up Flask service')
         yield
     finally:
-        print("\n stopping services")
+        logging.info("stopping services")
         subprocess.check_call(["docker-compose", "down"])
 
 
 @pytest.fixture(scope="module")
 def test_data(tmpdir_factory):
     """
-    create data files
+    create data files for testing
     """
-    print("creating data files")
+    logging.info("creating data files")
 
+    # list of files sizes to generate
     sizes = (100, 200, 300)
 
     files = []
     for size in sizes:
         filename = F"file{size}"
         filepath = tmpdir_factory.mktemp("data") / filename
-        gen_big_file(filepath, size)
+        generate_big_sparse_file(filepath, size)
         files.append(Path(filepath))
 
-    assert True
     return files
 
 
-def test_flask_server(services, test_data):
-    r = requests.request("GET", FLASK_HOST)
+def flask_ready_check(timeout=500, poll=5):
+    """
+    check readiness of flask server
 
+    Args:
+        timeout: max wait time in seconds
+        poll: polling time interval in seconds
+
+    Returns:
+        True if ready False otherwise
+
+
+    """
+
+    flask_ready = False
+
+    t_start = time()
+    while (time()-t_start) < timeout:
+        try:
+            r = requests.request("GET", FLASK_HOST)
+            if r.status_code == 200:
+                logging.info(F"FLASK server is ready, "
+                             F"took {round(time()-t_start,1)} seconds")
+                flask_ready = True
+                break
+        except requests.exceptions.ConnectionError:
+            sleep(poll)
+
+    return flask_ready
+
+
+def test_flask_server(services):
+    """
+    test if flask server is alive
+    """
+
+    r = requests.request("GET", FLASK_HOST)
     assert r.status_code == 200
 
 
 def test_upload(services, test_data, tmp_path):
-
+    """
+    test different end points in storage service
+    """
     test_files = test_data
 
     job_id = 12345
 
+    # test post operation
     for filepath in test_files:
         filename = filepath.name
         url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
@@ -109,6 +151,7 @@ def test_upload(services, test_data, tmp_path):
         r = requests.post(url, files=files)
         assert r.status_code == 201
 
+    # test get operation
     for filepath in test_files:
         filename = filepath.name
         url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
@@ -120,6 +163,7 @@ def test_upload(services, test_data, tmp_path):
 
         assert filecmp.cmp(download_path, filepath)
 
+    # test get operation on archive
     url = F'{STORAGE_ENDPOINT}/{job_id}'
     r = requests.get(url)
     download_dir = tmp_path / "downloaded"
@@ -142,35 +186,3 @@ def test_upload(services, test_data, tmp_path):
 
     assert r.status_code == DELETE_OK
 
-    # filepath = tmp_path / "bigfile1"
-    # filename = filepath.name
-    #
-    # gen_big_file(filepath, 100)
-    # job_id = 124
-    # url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
-    # files = {'file_data': filepath.open('rb')}
-    # r = requests.post(url, files=files)
-    # assert r.status_code == 201
-
-#
-# def test_upload(services, tmp_path):
-#
-#     filepath = tmp_path / "bigfile1"
-#     filename = filepath.name
-#
-#     gen_big_file(filepath, 100)
-#     job_id = 124
-#     url = F'{STORAGE_ENDPOINT}/{job_id}/{filename}'
-#     files = {'file_data': filepath.open('rb')}
-#     r = requests.post(url, files=files)
-#     assert r.status_code == 201
-#
-#     r = requests.get(url)
-#     filepath1 = tmp_path / "bigfile2"
-#     print("\n status code ", r.status_code)
-#     with filepath1.open('wb') as f:
-#         f.write(r.content)
-#
-#     assert filecmp.cmp(filepath1,filepath)
-#
-#
