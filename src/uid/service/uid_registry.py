c10a04b29077496a166b23cfc8272e423b9ce041
Elvis Offor <eoffor21@gmail.com>
2021-01-14 14:24:38 -0800

    ID registry: declare MinIO client once and reuse

--- src/uid/service/uid_registry.py
+++ src/uid/service/uid_registry.py
@@ -11,6 +11,16 @@ MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY')
 MINIO_SECRET_KEY = os.environ.get('MINIO_SECRET_KEY')
 AUTH_BUCKET_NAME = os.environ.get('MINIO_AUTH_BUCKET', 'auth')
 
+logging.debug(F"AUTH_BUCKET: {AUTH_BUCKET_NAME}")
+logging.debug(F"MINIO_URL:  {MINIO_URL}")
+
+MINIO_CLIENT = Minio(MINIO_URL,
+                    access_key=MINIO_ACCESS_KEY,
+                    secret_key=MINIO_SECRET_KEY,
+                    secure=False)
+
+if not MINIO_CLIENT.bucket_exists(AUTH_BUCKET_NAME):
+    MINIO_CLIENT.make_bucket(AUTH_BUCKET_NAME)
 
 def uid_register_job(job_id, metadata=None):
     """
@@ -27,28 +37,18 @@ def uid_register_job(job_id, metadata=None):
         job has been previously registered
 
     """
-    # TODO: 2020/07/02, Elvis - consider moving these log statements to avoid flooding of feed
-    logging.debug(F"AUTH_BUCKET: {AUTH_BUCKET_NAME}")
-    logging.debug(F"MINIO_URL:  {MINIO_URL}")
-    minioClient = Minio(MINIO_URL,
-                        access_key=MINIO_ACCESS_KEY,
-                        secret_key=MINIO_SECRET_KEY,
-                        secure=False)
-
-    if not minioClient.bucket_exists(AUTH_BUCKET_NAME):
-        minioClient.make_bucket(AUTH_BUCKET_NAME)
 
     object_name = str(job_id)
 
     # look for previous registration
     try:
-        minioClient.stat_object(AUTH_BUCKET_NAME, object_name)
+        MINIO_CLIENT.stat_object(AUTH_BUCKET_NAME, object_name)
         logging.error(F"Job id {object_name} has been registered previously")
         raise ValueError
     except NoSuchKey as err:
         pass
 
-    minioClient.put_object(AUTH_BUCKET_NAME, object_name,
+    MINIO_CLIENT.put_object(AUTH_BUCKET_NAME, object_name,
                            data=io.BytesIO(),
                            length=0,
                            metadata=metadata)
@@ -67,7 +67,8 @@ def uid_void_job(job_id):
         If job identifier is not found only warning will logged
 
     """
-    minioClient = Minio(MINIO_URL,
+    # minioClient = Minio(MINIO_URL,
+    MINIO_CLIENT = Minio(MINIO_URL,
                         access_key=MINIO_ACCESS_KEY,
                         secret_key=MINIO_SECRET_KEY,
                         secure=False)
@@ -76,11 +77,13 @@ def uid_void_job(job_id):
 
     # look for previous registration
     try:
-        minioClient.stat_object(AUTH_BUCKET_NAME, object_name)
+        # minioClient.stat_object(AUTH_BUCKET_NAME, object_name)
+        MINIO_CLIENT.stat_object(AUTH_BUCKET_NAME, object_name)
     except NoSuchKey as err:
         logging.warning(F"Received request to delete unknown job {object_name}")
 
-    minioClient.remove_object(AUTH_BUCKET_NAME, object_name)
+    # minioClient.remove_object(AUTH_BUCKET_NAME, object_name)
+    MINIO_CLIENT.remove_object(AUTH_BUCKET_NAME, object_name)
 
 
 def uid_validate_job(job_id):
@@ -96,7 +99,7 @@ def uid_validate_job(job_id):
 
     """
 
-    minioClient = Minio(MINIO_URL,
+    MINIO_CLIENT = Minio(MINIO_URL,
                         access_key=MINIO_ACCESS_KEY,
                         secret_key=MINIO_SECRET_KEY,
                         secure=False)
@@ -104,7 +107,8 @@ def uid_validate_job(job_id):
     object_name = str(job_id)
 
     try:
-        obj = minioClient.stat_object(AUTH_BUCKET_NAME, object_name)
+        # obj = minioClient.stat_object(AUTH_BUCKET_NAME, object_name)
+        obj = MINIO_CLIENT.stat_object(AUTH_BUCKET_NAME, object_name)
         metadata = {"created_dt": obj.last_modified, **obj.metadata}
         metadata.pop("Content-Type",None)
     except NoSuchKey as err:
